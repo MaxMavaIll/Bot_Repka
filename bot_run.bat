0f989a2e721ed501481d6ebf06433b113d062091
Illya <illvauser8@gmail.com>
2022-04-07 09:28:03 +0300

    Бот 2.0

---
+++ bot_run.bat
@@ -0,0 +1,12 @@
+@echo off
+
+cd /D D:\TelegramAIO
+
+call D:\TelegramAIO\venv\Scripts\activate
+
+set TOKEN=1977376381:AAEESspve1o3gPVCNNjvy6M68u-yRuQnyb8
+
+
+python bot_telegram.py
+
+pause
---
+++ config.py
@@ -0,0 +1,10 @@
+TOKEN_REPKA = "1977376381:AAEESspve1o3gPVCNNjvy6M68u-yRuQnyb8"
+TOKEN_TEST = "1258575025:AAG2Bxi1xPlsxEUM-ExP1ejBXBrePATnnoQ"
+URL_APP = "https://repkabot.herokuapp.com/"
+
+blok = {}
+
+# Макс, Ілля, Марго, Женя
+admin = [592497023, 954878160, 706683490, 513171946]
+
+password = "1234"
